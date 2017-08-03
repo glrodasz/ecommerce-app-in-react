e37f9d32905ae264eb32d0e09899f287c7eb0246
Guillermo Rodas <garethderioth@gmail.com>
2017-08-03 18:55:57 -0500

    Cards boilerplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import Child from './Child';
 import { Container } from 'semantic-ui-react';
 
 class App extends Component {
     render() {
+      const cards = [
+        {
+          title: "Guillermo",
+          date: new Date(),
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          friends: 300
+        }
+      ]
       return (
         <Container text>
-          <h1>I got rendered!</h1>
-          <Child />
+          {/* TODO: print cards */}
         </Container>
       );
     }
